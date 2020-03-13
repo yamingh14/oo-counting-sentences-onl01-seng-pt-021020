50ac6ec6e74e8ccc326f1f5af1332b62bef39d2c
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-13 18:55:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
+class String
+
+  def sentence?
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
+  end
+
+  def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
+  end
+
+  def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
+  end
+
+  def count_sentences
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
+  end
+end
 # class String
 #
 #   def sentence?
