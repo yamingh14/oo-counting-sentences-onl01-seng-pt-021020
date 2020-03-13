be9b66436bde48b171e1758487a6a12f989a8d9f
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-13 18:55:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,18 +1,18 @@
 # class String
-# 
+#
 #   def sentence?
-# 
+#
 #   end
-# 
+#
 #   def question?
-# 
+#
 #   end
-# 
+#
 #   def exclamation?
-# 
+#
 #   end
-# 
+#
 #   def count_sentences
-# 
+#
 #   end
 # end
